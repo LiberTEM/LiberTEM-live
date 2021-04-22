f9746e3d5800d266063e40bb3a86febbcfa8d140
Dieter Weber <d.weber@fz-juelich.de>
2021-04-22 19:55:19 +0200

    Restructure the dataset and context manager code
    * Setup routine is part of the dataset, not the Context
    * Plain constructor for dataset, don't require separate construction
    of the data source
    
    The setup routine now behaves similar to an overridden method in a subclass of the dataset.
    The API should be further refined, probably for K2IS.

--- src/libertem_live/api.py
+++ src/libertem_live/api.py
@@ -5,16 +5,54 @@ from libertem.api import Context
 
 
 class LiveContext(Context):
-    def __init__(self, camera_setup, executor=None):
+    def __init__(self, executor=None):
         if executor is None:
             executor = InlineJobExecutor()
         super().__init__(executor=executor)
-        self._camera_setup = camera_setup
 
     @contextlib.contextmanager
     def _do_camera_setup(self, dataset, udf):
-        with self._camera_setup(dataset, udf):
+        if hasattr(dataset, 'camera_setup'):
+            with dataset.camera_setup(dataset, udf):
+                yield
+        else:
             yield
+
+    def prepare_acquisition(self, detector_type, camera_setup, *args, **kwargs):
+        # FIXME implement similar to LiberTEM datasets once
+        # we have more detector types to support
+        '''
+        Prepare a live data set for acquisition
+
+        Parameters
+        ----------
+        detector_type : str
+            - :code:`'merlin'`: Quantum Detectors Merlin camera. Additional parameters:
+        camera_setup : contextmanager
+            Context manager to initialize the camera and tear it down.
+            This can be used to send the commands to the camera and
+            other parts of the setup to start a scan and clean up afterwards.
+            It will be entered when an acquisition is started.
+            Parameters supplied to the context manager are the live dataset and a list of UDFs.
+        * args, **kwargs
+            Additional parameters for the acquisition, see below
+
+        Merlin Parameters
+        -----------------
+
+        * :code:`scan_size`: tuple(int)
+        * :code:`host`: str, hostname of the Merlin data server
+        * :code:`port`: int, port of the Merlin data server
+        * :code:`frames_per_partition`: int
+        * :code:`pool_size`: int, number of decoding threads. Defaults to 2
+        '''
+        detector_type = str(detector_type).lower()
+        if detector_type == 'merlin':
+            from libertem_live.detectors.merlin.dataset import MerlinLiveDataSet
+            cls = MerlinLiveDataSet
+        else:
+            raise ValueError(f"Unknown detector type '{detector_type}', supported is 'merlin'")
+        return cls(camera_setup, *args, **kwargs).initialize(self.executor)
 
     def run_udf(self, dataset, udf, *args, **kwargs):
         if not isinstance(udf, (list, tuple)):
---
+++ src/libertem_live/udf/monitor.py
@@ -0,0 +1,30 @@
+from libertem.udf import UDF
+
+
+class MonitorUDF(UDF):
+    '''
+    Return the most recently processed frame.
+
+    This is useful for live processing as a beam monitor. Individual frames in
+    an offline dataset are more easily accessible with
+    :class:`~libertem.udf.raw.PickUDF`.
+
+    The result is most likely the last frame of each partition for the
+    individual merge steps. The end result depends on the merge order.
+    '''
+    
+    def get_preferred_input_dtype(self):
+        return self.USE_NATIVE_DTYPE
+    
+    def get_result_buffers(self):
+        return {
+            "intensity": self.buffer(kind='sig', dtype=self.meta.input_dtype)
+        }
+    
+    def process_tile(self, tile):
+        # Assign the portion from the last frame within the tile
+        # to the result buffer
+        self.results.intensity[:] = tile[-1]
+        
+    def merge(self, dest, src):
+        dest.intensity[:] = src.intensity
