f86d5b7e092a863efe9238f3ad995980785d7615
Dieter Weber <d.weber@fz-juelich.de>
2021-05-26 18:47:19 +0200

    Test coverage for Merlin
    * Allow running simulators within test fixtures
    * Use test data if available
    
    Code that was unused is commented out for now.

---
+++ tests/utils.py
@@ -0,0 +1,10 @@
+import os
+
+
+def get_testdata_path():
+    return os.environ.get(
+        'TESTDATA_BASE_PATH',
+        os.path.normpath(
+            os.path.join(os.path.dirname(__file__), '..', 'data')
+        )
+    )
