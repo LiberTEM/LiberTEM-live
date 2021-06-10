cc6e40be193b2d6a8636acb0f7ae2bdc85d8ea09
Dieter Weber <d.weber@fz-juelich.de>
2021-06-10 14:59:34 +0200

    0.7 is released
    Co-authored-by: Alexander Clausen <a.clausen@fz-juelich.de>

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     zip_safe=False,
     python_requires='>=3.6',
     install_requires=[
-        "libertem>=0.7.0.dev0",
+        "libertem>=0.7.0",
         "numpy",
         "click",
         "tqdm",
