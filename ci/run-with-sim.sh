2ff5fc08b12bce9e366c262cfaa68549a9afe7da
Alexander Clausen <alex@gc-web.de>
2022-08-22 18:47:12 +0200

    Also run the DECTRIS simulator in the background

--- ci/run-with-sim.sh
+++ ci/run-with-sim.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 # run both nbval and libertem-live-mib-sim
+# assumes default ports are usable (i.e. isolated environment)
 
 .tox/notebooks/bin/libertem-live-mib-sim --host 127.0.0.1 "$TESTDATA_BASE_PATH/20200518 165148/default.hdr" --cached=MEMFD --wait-trigger&
-SIM_PID=$!
+MERLIN_SIM_PID=$!
+
+.tox/notebooks/bin/libertem-live-dectris-sim "$TESTDATA_BASE_PATH/dectris/zmqdump.dat.128x128-id34-exte-bslz4" --port 8910 --zmqport 9999&
+DECTRIS_SIM_PID=$!
 
 cleanup() {
-    kill "$SIM_PID"
+    kill "$MERLIN_SIM_PID"
+    kill "$DECTRIS_SIM_PID"
 }
 
 trap cleanup EXIT
 
-echo "started simulator in background, pid=$SIM_PID"
-echo "waiting for data socket to be ready..."
+echo "started merlin simulator in background, pid=$MERLIN_SIM_PID"
+echo "started dectris simulator in background, pid=$DECTRIS_SIM_PID"
+echo "waiting for data sockets to be ready..."
 
-while ! (echo "" > /dev/tcp/127.0.0.1/6342) 2>/dev/null; do echo "Waiting..."; sleep 1; done
+while ! (echo "" > /dev/tcp/127.0.0.1/6342) 2>/dev/null; do echo "Waiting for 6342..."; sleep 1; done
+while ! (echo "" > /dev/tcp/127.0.0.1/8910) 2>/dev/null; do echo "Waiting for 8910..."; sleep 1; done
+while ! (echo "" > /dev/tcp/127.0.0.1/9999) 2>/dev/null; do echo "Waiting for 9999..."; sleep 1; done
 
-echo "done waiting, data socket ready!"
+echo "done waiting, sockets ready!"
 
 "$@"
