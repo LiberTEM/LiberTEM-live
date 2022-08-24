cfc5d71383b5d27c9ac0277f7b57ab7d482d67b0
Alexander Clausen <alex@gc-web.de>
2022-08-24 18:43:22 +0200

    Remove dependency on bitshuffle package
    Needs https://github.com/LiberTEM/LiberTEM-dectris-rs/pull/10

--- setup.py
+++ setup.py
@@ -90,7 +90,6 @@ setup(
         'pytango': 'pytango',
         'dectris': [
             "libertem-dectris>=0.2; python_version>='3.7'",
-            "bitshuffle @ git+https://github.com/uellue/bitshuffle.git@nohdf5#egg=bitshuffle",
             "lz4",
         ]
     },
