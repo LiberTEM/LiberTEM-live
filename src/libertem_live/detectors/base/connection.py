786673220e3ac78753130a73c6838d78b52801e3
Alexander Clausen <alex@gc-web.de>
2023-01-27 11:19:44 +0100

    add missing file

---
+++ src/libertem_live/detectors/base/connection.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+class PendingAcquisition:
+    def get_acquisition(self, *args, trigger=None, **kwargs):
+        raise NotImplementedError()
+
+
+class DetectorConnection:
+    def wait_for_acquisition(self, timeout: Optional[float] = None) -> PendingAcquisition:
+        """
+        Wait for at most `timeout` seconds for an acquisition to start. This
+        does not perform any triggering itself and expects something external
+        to arm and trigger the acquisition.
+
+        Once the detector is armed, this function returns a `PendingAcquisition`,
+        which can be converted to a full `Acquisition` object.
+
+        The function returns `None` on timeout.
+
+        Parameters
+        ----------
+        timeout : Optional[float]
+            Timeout in seconds. If `None`, wait indefinitely.
+        """
+        raise NotImplementedError()
