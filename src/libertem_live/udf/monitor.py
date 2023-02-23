b16a7736e1ffbedc523a8065cf2ab67709d05af9
Dieter Weber <d.weber@fz-juelich.de>
2023-02-23 15:05:11 +0100

    Partition monitor UDF; native sparse processing

--- src/libertem_live/udf/monitor.py
+++ src/libertem_live/udf/monitor.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from libertem.udf import UDF
 
 
@@ -13,6 +15,12 @@ class SignalMonitorUDF(UDF):
     individual merge steps. The end result depends on the merge order.
     '''
 
+    def get_backends(self):
+        return (
+            backend for backend in self.BACKEND_ALL
+            if backend not in {self.BACKEND_CUPY_SCIPY_COO, self.BACKEND_SCIPY_COO}
+        )
+
     def get_preferred_input_dtype(self):
         ''
         return self.USE_NATIVE_DTYPE
@@ -20,14 +28,67 @@ class SignalMonitorUDF(UDF):
     def get_result_buffers(self):
         ''
         return {
-            "intensity": self.buffer(kind='sig', dtype=self.meta.input_dtype)
+            "intensity": self.buffer(kind='sig', dtype=self.meta.input_dtype, where='device')
         }
 
     def process_tile(self, tile):
         ''
         # Assign the portion from the last frame within the tile
         # to the result buffer
-        self.results.intensity[:] = tile[-1]
+        self.results.intensity[:] = self.forbuf(tile[-1], self.results.intensity)
+
+    def merge(self, dest, src):
+        ''
+        dest.intensity[:] = src.intensity
+
+
+# Largely copied from SumUDF, but separate implementation is
+# desirable, for example since `merge_all()` doesn't make sense
+# here.
+class PartitionMonitorUDF(UDF):
+    """
+    Sum up frames in a partition and update result with
+    the latest partition result.
+
+    This is useful for live processing as a beam monitor if
+    individual frames contain not enough signal. Partial sums of
+    an offline dataset are more easily accessible with
+    :class:`~libertem.udf.sum.SumUDF` with a ROI.
+
+    The result is the sum of the partition that was merged last.
+    The end result depends on the merge order.
+
+    Parameters
+    ----------
+    dtype : numpy.dtype, optional
+        Preferred dtype for computation, default 'float32'. The actual dtype will be determined
+        from this value and the dataset's dtype using :meth:`numpy.result_type`.
+        See also :ref:`udf dtype`.
+
+    """
+    def __init__(self, dtype='float32'):
+        super().__init__(dtype=dtype)
+
+    def get_preferred_input_dtype(self):
+        return self.params.dtype
+
+    def get_backends(self):
+        return self.BACKEND_ALL
+
+    def get_result_buffers(self):
+        ''
+        return {
+            'intensity': self.buffer(
+                kind='sig', dtype=self.meta.input_dtype, where='device'
+            )
+        }
+
+    def process_tile(self, tile):
+        ''
+        self.results.intensity[:] += self.forbuf(
+            np.sum(tile, axis=0),
+            self.results.intensity
+        )
 
     def merge(self, dest, src):
         ''
--- tests/udf/test_monitor.py
+++ tests/udf/test_monitor.py
@@ -1,14 +1,55 @@
+import pytest
 import numpy as np
+from numpy.testing import assert_allclose
+from sparseconverter import get_device_class
+from libertem.udf.sum import SumUDF
 
-from libertem_live.udf.monitor import SignalMonitorUDF
+from libertem_live.udf.monitor import SignalMonitorUDF, PartitionMonitorUDF
+
+from utils import set_device_class
 
 
-def test_monitor(ltl_ctx):
-    data = np.random.random((13, 17, 19, 23))
-    aq = ltl_ctx.prepare_acquisition('memory', trigger=None, data=data)
+@pytest.mark.parametrize(
+    'backend', tuple(SignalMonitorUDF().get_backends()) + (None, )
+)
+def test_monitor(ltl_ctx, backend):
+    with set_device_class(get_device_class(backend)):
+        if backend is None:
+            backends = None
+        else:
+            backends = (backend, )
+        data = np.random.random((13, 17, 19, 23))
+        aq = ltl_ctx.prepare_acquisition('memory', trigger=None, data=data, array_backends=backends)
 
-    udf = SignalMonitorUDF()
+        udf = SignalMonitorUDF()
 
-    res = ltl_ctx.run_udf(dataset=aq, udf=udf)
+        res = ltl_ctx.run_udf(dataset=aq, udf=udf)
 
-    assert np.all(res['intensity'].data == data[-1, -1])
+        assert np.all(res['intensity'].data == data[-1, -1])
+
+
+@pytest.mark.parametrize(
+    'backend', tuple(PartitionMonitorUDF().get_backends()) + (None, )
+)
+@pytest.mark.parametrize(
+    'udf_kwargs', ({}, {'dtype': np.complex128})
+)
+def test_partition_monitor(ltl_ctx, backend, udf_kwargs):
+    with set_device_class(get_device_class(backend)):
+        if backend is None:
+            backends = None
+        else:
+            backends = (backend, )
+        data = np.random.random((13, 17, 19, 23))
+        aq = ltl_ctx.prepare_acquisition('memory', trigger=None, data=data, array_backends=backends)
+
+        udf = PartitionMonitorUDF(**udf_kwargs)
+
+        dtype = udf_kwargs.get('dtype')
+        res = np.zeros(aq.shape.sig, dtype=dtype)
+        for part_res in ltl_ctx.run_udf_iter(dataset=aq, udf=udf):
+            res += part_res.buffers[0]['intensity'].raw_data
+
+        ref = ltl_ctx.run_udf(dataset=aq, udf=SumUDF())
+
+        assert_allclose(ref['intensity'].raw_data, res)
