ce77fc8644274cda783b1f2fa73ca505e9ca644a
Alexander Clausen <alex@gc-web.de>
2023-03-30 20:30:55 +0200

    Port the memory dataset to the new API

--- tests/udf/test_monitor.py
+++ tests/udf/test_monitor.py
@@ -2,8 +2,10 @@ import pytest
 import numpy as np
 from numpy.testing import assert_allclose
 from sparseconverter import get_device_class
+
 from libertem.udf.sum import SumUDF
 
+from libertem_live.api import LiveContext
 from libertem_live.udf.monitor import SignalMonitorUDF, PartitionMonitorUDF
 
 from utils import set_device_class
@@ -12,20 +14,22 @@ from utils import set_device_class
 @pytest.mark.parametrize(
     'backend', tuple(SignalMonitorUDF().get_backends()) + (None, )
 )
-def test_monitor(ltl_ctx, backend):
+def test_monitor(ltl_ctx: LiveContext, backend):
     with set_device_class(get_device_class(backend)):
         if backend is None:
             backends = None
         else:
             backends = (backend, )
         data = np.random.random((13, 17, 19, 23))
-        aq = ltl_ctx.prepare_acquisition('memory', trigger=None, data=data, array_backends=backends)
-
-        udf = SignalMonitorUDF()
-
-        res = ltl_ctx.run_udf(dataset=aq, udf=udf)
-
-        assert np.all(res['intensity'].data == data[-1, -1])
+        conn = ltl_ctx.make_connection('memory').open(
+            data=data,
+            extra_kwargs=dict(array_backends=backends)
+        )
+        with conn:
+            aq = ltl_ctx.make_acquisition(conn=conn)
+            udf = SignalMonitorUDF()
+            res = ltl_ctx.run_udf(dataset=aq, udf=udf)
+            assert np.all(res['intensity'].data == data[-1, -1])
 
 
 @pytest.mark.parametrize(
@@ -34,22 +38,27 @@ def test_monitor(ltl_ctx, backend):
 @pytest.mark.parametrize(
     'udf_kwargs', ({}, {'dtype': np.complex128})
 )
-def test_partition_monitor(ltl_ctx, backend, udf_kwargs):
+def test_partition_monitor(ltl_ctx: LiveContext, backend, udf_kwargs):
     with set_device_class(get_device_class(backend)):
         if backend is None:
             backends = None
         else:
             backends = (backend, )
         data = np.random.random((13, 17, 19, 23))
-        aq = ltl_ctx.prepare_acquisition('memory', trigger=None, data=data, array_backends=backends)
 
-        udf = PartitionMonitorUDF(**udf_kwargs)
+        conn = ltl_ctx.make_connection('memory').open(
+            data=data,
+            extra_kwargs=dict(array_backends=backends)
+        )
+        with conn:
+            aq = ltl_ctx.make_acquisition(conn=conn)
+            udf = PartitionMonitorUDF(**udf_kwargs)
 
-        dtype = udf_kwargs.get('dtype')
-        res = np.zeros(aq.shape.sig, dtype=dtype)
-        for part_res in ltl_ctx.run_udf_iter(dataset=aq, udf=udf):
-            res += part_res.buffers[0]['intensity'].raw_data
+            dtype = udf_kwargs.get('dtype')
+            res = np.zeros(aq.shape.sig, dtype=dtype)
+            for part_res in ltl_ctx.run_udf_iter(dataset=aq, udf=udf):
+                res += part_res.buffers[0]['intensity'].raw_data
 
-        ref = ltl_ctx.run_udf(dataset=aq, udf=SumUDF())
+            ref = ltl_ctx.run_udf(dataset=aq, udf=SumUDF())
 
-        assert_allclose(ref['intensity'].raw_data, res)
+            assert_allclose(ref['intensity'].raw_data, res)
