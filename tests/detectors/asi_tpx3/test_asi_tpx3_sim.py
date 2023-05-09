8714e5525071658192a9dcfa4e0a197b1e745b5a
Alexander Clausen <alex@gc-web.de>
2023-05-09 13:17:53 +0200

    Fix for nav_shape != (32, 32) in asi tpx3 sim

--- tests/detectors/asi_tpx3/test_asi_tpx3_sim.py
+++ tests/detectors/asi_tpx3/test_asi_tpx3_sim.py
@@ -75,3 +75,26 @@ def test_memfd_mock_data(ctx_pipelined: LiveContext):
             )
             res = ctx_pipelined.run_udf(dataset=aq, udf=SumSigUDF())
             assert not np.allclose(res['intensity'], 0)
+
+
+def test_mock_data_shape(ctx_pipelined: LiveContext):
+    sim_ctx_mgr = contextmanager(run_camera_sim)
+
+    with sim_ctx_mgr(
+        cls=TpxCameraSim,
+        mock_nav_shape=(64, 64),
+        port=0,
+        sleep=0.1,
+        cached='MEM',
+    ) as tpx_sim:
+        with ctx_pipelined.make_connection('asi_tpx3').open(
+            data_port=tpx_sim.server_t.port,
+        ) as conn:
+            pending_aq = conn.wait_for_acquisition(10.0)
+            assert pending_aq is not None
+            aq = ctx_pipelined.make_acquisition(
+                conn=conn,
+                pending_aq=pending_aq,
+            )
+            res = ctx_pipelined.run_udf(dataset=aq, udf=SumSigUDF())
+            assert not np.allclose(res['intensity'], 0)
