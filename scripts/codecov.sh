1e384b2dc83ba148744cf4a64710f85d63030dcf
Alexander Clausen <alex@gc-web.de>
2023-07-24 20:05:33 +0200

    Update codecov uploader binary version
    Port of https://github.com/LiberTEM/LiberTEM/pull/1471

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -3,8 +3,8 @@
 set -e
 
 # pin the version so we can compare sha
-curl -Os https://uploader.codecov.io/v0.3.2/linux/codecov
-echo '20f9c9d78483fce977b6cc39e231a734a23bcd36f4d536bb7355222fb88d02bc codecov' | sha256sum -c
+curl -Os https://uploader.codecov.io/v0.6.1/linux/codecov
+echo '0c9b79119b0d8dbe7aaf460dc3bd7c3094ceda06e5ae32b0d11a8ff56e2cc5c5 codecov' | sha256sum -c
 
 chmod +x codecov
 ./codecov $*
